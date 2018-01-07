a4a69465e69d9e744d6ea7d9cdedcc9b7efe94ac
Anton Strömkvist <anton@stromkvist.com>
2018-01-07 13:30:28 +0100

    Prevent JS from being mean when hot/live reloading

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const app = Main.embed(document.getElementById("root"));
 
 app.ports.setupScrollSpy.subscribe(sections => {
   window.addEventListener("scroll", e => {
-    const activeHash = sections.find(
-      id => document.querySelector(id).offsetTop <= e.pageY
-    );
+    const activeHash =
+      sections.find(
+        id => (document.querySelector(id) || {}).offsetTop <= e.pageY
+      ) || "#home";
     window.history.replaceState(null, null, activeHash);
     app.ports.activeHash.send(activeHash);
   });
